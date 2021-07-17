7f55f9dea6794ea986ece30dda427a647df0aa16
M Fadhilah Rio <muhamadfrio@gmail.com>
2021-07-17 19:45:51 +0700

    Cart page title

--- application/views/cart/index.php
+++ application/views/cart/index.php
@@ -10,7 +10,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1><?= $page_name; ?></h1>
+            <h1><?= ($this->input->get('type') == 'shopping') ? $page_name.' Belanja' : $page_name.' Servis' ?></h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
